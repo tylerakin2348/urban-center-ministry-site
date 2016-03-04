c9521c3b131936f60773a7f073effe24e2c3f981
tylerakin2348 <takin963@students.sbts.edu>
2016-03-04 16:28:34 -0500

    Responsive, Style, and Interaction Changes
    This commit is primarily a step in figuring out an ajax request.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,93 @@
-$("button").click(function() {
-  $(".main-navigation").toggle();
+var searchValue = $("#search").val();
+
+// Navigation Menu Functionality
+$(".topHeaderScroll").hide();
+$(window).scroll(function() {
+  $(".top-header").hide();
+  $(".topHeaderScroll").show();
 });
 
+$(".navButton").click(function() {
+  if ( $(".main-navigation").is(":hidden") ) {
+  $(".main-navigation").slideDown("slow");
+} else {
+  $(".main-navigation").slideUp("fast");
+}
+});
+
+$(".navButton").click(function() {
+  if ( $(".mainNavigationScroll").is(":hidden") ) {
+  $(".mainNavigationScroll").slideDown("slow");
+} else {
+  $(".mainNavigationScroll").slideUp("fast");
+}
+});
+
+//Search Form Request
+$( "#search" ).keyup(function() {
+  searchValue = $("#search").val();
+    var found = $.grep(data.resources, function(resource) {
+        return resource.author === searchValue;
+    });
+    if(response != ''){
+      $(".search-overlay").show();
+    }
+  });
+
+  //Contact Form Submit Request
+  $("#contactSubmitButton").click(function( submit ) {
+    submit.preventDefault();
+    $.get()
+  });
+
+  $.getJSON("https://raw.githubusercontent.com/tylerakin2348/urban-center-ministry-site/master/data.json", function( response ) {
+    var data;
+    data = rsesponse;
+  });
+
+
+
+
+
+
+  // $(".contact").append(".contactForm");
+  //
+  // $("#searchButton").click(function() {
+  //
+  // });
+  // $("form").onclick
+//   $(window).bind('scroll', function () {
+//     if ($(window).scrollTop() > 100) {
+//         $('.menu').addClass('fixed');
+//     } else {
+//         $('.menu').removeClass('fixed');
+//     }
+// });
+
+
+  // $.getJSON("data.json", function(json) {
+  //   console.log("JSON Data: " + data.json[0].name);
+  // });
+
+
+  // $.getJSON("data.json", function (data) {
+  //   $.each(data, function (index, source) {
+  //     if (source.author === ("#search").val()
+  // });
+
+
+// searchValue = $("#search").val();
+// });
+// $( "#search" ).keyup(function() {
+//   console.log(searchValue)
+// });
+//
+//
+// $( "#search" ).keyup(function() {
+
+// $("button").click(function() {
+//   $(".main-navigation").toggle();
+// });
 
 
 
