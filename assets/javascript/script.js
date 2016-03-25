ebef4593a311f245412608213b8a9e2f31c88955
tylerakin2348 <takin963@students.sbts.edu>
2016-03-25 11:57:05 -0400

    Final Commit
    Here is my final Commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,20 +1,61 @@
 var searchValue = $("#search").val();
-// var ajaxCall = function(ajaxCall) {
+
+//Ajax Call for API
   $.ajax({
       url:'https://api.instagram.com/v1/users/196756729/media/recent/?access_token=196756729.43505f7.bc26468655ad47979220f2a18f3fc6c0',
       dataType:'jsonp',
       jsonp:'callback',
       success: function (data) {
         var sbtsimages = data.data;
-          for(i = 0; i < sbtsimages.length; i +=1) {
-            $('#aboutSouthern').append('<img src="' + Object[i].object.images.thumbnail.url + '" />');
-            console.log(data);
+          for(i = 10; i < sbtsimages.length; i +=3) {
+            $('#aboutSouthernPhotos').append('<img src="' + sbtsimages[i].images.thumbnail.url + '" />');
           }
       }
   });
 
+//Toggle Ajax-call photos and slide page down to them when clicked
+$(".showPhotoButton").click(function() {
+    $('html, body').animate({
+      scrollTop: $(this).offset().top
+    }, 1000);
+});
 
-// Desktop Navigation Menu Functionality
+
+// Not using code, but code worked when in use. I was working on developing a search bar, which responds to certain statements, thus the show/hide fucntionality of the result bar.
+$( "#search" ).keyup(function() {
+  searchValue = $("#search").val();
+    var found = $.grep(data.author, function(resource) {
+        return data.author === searchValue;
+    });
+    if(searchValue = data.author ){
+      $(".search-overlay").show();
+    } else  {
+      $(".search-overlay").hide();
+    }
+  });
+
+    $.ajax({
+      type: "POST",
+      url: $("form").attr("action"),
+      data: "formDataList"
+    });
+$(document).ready(function() {
+  $(".contactForm").submit(function(evt) {
+    evt.preventDefault();
+    var url = ("https://raw.githubusercontent.com/tylerakin2348/urban-center-ministry-site/master/data.json");
+    var formData = $(".contactForm").val();
+    $.post(url, formData, function(response) {
+    $("#contactSubmitButton").hide();
+    alert("Thank you for submitting!");
+    });
+  }); // end submit
+});
+
+
+
+
+
+// Desktop Navigation Menu Functionality - Figured out and written by mentor while I looked on - included because it's awesome.
 var scrollStatus = false;
 
 $(window).scroll(function() {
@@ -23,7 +64,7 @@ $(window).scroll(function() {
       logoCon = $('.logo-container'),
       logo = $('.logo');
 
-  if(scrollStatus && top == 0)
+  if(scrollStatus && top <= 10)
   {
     scrollStatus = false;
     logo.animate({
@@ -31,11 +72,11 @@ $(window).scroll(function() {
     }, 200);
     logoCon.animate({
       height: 164,
-      marginTop:0
+      marginTop:3
     }, 200);
   }
 
-  if(!scrollStatus && top > 0)
+  if(!scrollStatus && top > 10)
   {
     scrollStatus = true;
     logo.animate({
@@ -47,86 +88,3 @@ $(window).scroll(function() {
     }, 200);
   }
 });
-
-// Mobile Navigation Menu Functionality
-$(".navButton").click(function() {
-  if ( $(".main-navigation").is(":hidden") ) {
-  $(".main-navigation").slideDown("slow");
-} else {
-  $(".main-navigation").slideUp("fast");
-}
-});
-
-// Search Form AJAX Request
-$( "#search" ).keyup(function() {
-  searchValue = $("#search").val();
-    if(searchValue === data.resources[0].author){
-      alert(data.resources[0].author);
-    } else {
-      if(searchValue === data.resources[0].category){
-        alert(data.resources[0].category);
-      }
-    }
-  });
-
-  // Contact Form Submit Request
-  $(document).ready(function() {
-    $(".contactForm").submit(function(evt) {
-      evt.preventDefault();
-      var url = ("https://raw.githubusercontent.com/tylerakin2348/urban-center-ministry-site/master/data.json");
-      var formData = $(".contactForm").val();
-      $.post(url, formData, function(response) {
-      $("#contactSubmitButton").hide();
-      });
-    }); // end submit
-  });
-
-  $(document).ready(function() {
-    $("#AjaxButton").show();
-    $("#AjaxButton").click(function(evt) {
-      evt.preventDefault();
-      var url = ("https://raw.githubusercontent.com/tylerakin2348/urban-center-ministry-site/master/data.json");
-      var formData = $("url");
-      $.getJSON(url, formData, function(data) {
-        // $.each(formData, function(success, data) {
-        //
-        // });
-        console.log(data);
-      });
-    }); // end submit
-  });
-
-//Search Interactivity
-  $(document).ready(function() {
-    var url = ("https://raw.githubusercontent.com/tylerakin2348/urban-center-ministry-site/master/data.json");
-    var searchData = $("url");
-
-    $( "#search" ).keyup(function() {
-
-      $.getJSON(url, searchData, function(searchResult) {
-        // $.each(formData, function(success, data) {
-        //
-        // });
-        console.log(searchResult);
-        });
-      });
-    }); // end submit
-
-  //Contact Form Submit Request
-    // // submit.preventDefault();
-    // var contactValue = $(".contactForm");
-    // var formData = $("formData");
-    // var formDataList = "";
-    // $("form").submit(function(event) {
-    //   // Stop the browser from submitting the form.
-    //   event.preventDefault();
-    //   var formDataList = $("form").serialize();
-    // });
-    // $.ajax({
-    //   type: "POST",
-    //   url: $("form").attr("action"),
-    //   data: "formDataList"
-    // })
-    // .done(function() {
-    //   //alert(formDataList.author);
-// });
--- assets/javascript/vueSite.js
+++ assets/javascript/vueSite.js
@@ -1,74 +1,18 @@
+// Mobile Navigation Menu Functionality
+
 new Vue({
-  el: '#formData',
+  el: '.navButton',
   data: {
 
-  }
-},
+  },
   methods: {
-    validate: function () {
-      this.message = this.message.split('').reverse().join('')
+    toggleMenu: function (downUp) {
+        if ( $(".main-navigation").is(":hidden") ) {
+        $(".main-navigation").slideDown("slow");
+      } else {
+        $(".main-navigation").slideUp("fast");
+      }
+
     }
   }
-})
-
-new Vue({
-  el: '.contactEmail',
-  data: {
-
-  }
-},
-  methods: {
-    validate: function () {
-      this.message = this.val();
-      if(this.message = "");
-    }
-  }
-})
-
-
-
-
-new Vue({
-  ready: function () {
-    var resource = this.$resource();
-    // get item
- resource.get({id: 1}).then(function (response) {
-     this.$set('item', response.item)
- });
-
-  }
-})
-
-
-
-
-
-
-
-
-
-
-// Vue.component("tasks", {
-//   template: "#testVue",
-//
-//   data: function() {
-//
-//     return {
-//         list: []
-//     };
-//   },
-//
-//   created: function() {
-//     this.fetchTaskList();
-//   },
-//
-//   methods: {
-//     fetchTaskList: function() {
-//       $.getJSON
-//     }
-//   }
-// })
-// new Vue({
-
-//Search Call
-//Return matching search results when input into search bar
+});
